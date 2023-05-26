4f0a21f80f07bd4f1fdde6f7250497cb38ec859c
Juhi Srivastava <jsrivastava@hpe.com>
2023-05-26 12:20:13 +0530

    Add project files.

---
+++ LoginPortal/wwwroot/js/fetchService.js
@@ -0,0 +1,33 @@
+﻿
+
+function get(url)
+{
+    let  req = fetch(url, {
+        method: 'get'
+    })
+    return req
+}
+
+function post(url, data) {
+    let req = fetch(url, {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    return req
+}
+
+function upload(url, data) {
+    let req = fetch(url, {
+        method: 'post',
+        body: data
+    })
+    return req
+}
+
+function processResponse(response)
+{
+    return response.json()
+}
---
+++ LoginPortal/wwwroot/js/login-index.js
@@ -0,0 +1,37 @@
+﻿const users = [];
+
+function passLoginData() {
+
+    var emailId = document.getElementById('email-id-input').value
+    var passwordID = document.getElementById('password-input').value
+    var tokenID = 0;
+    console.log(emailId, passwordID)
+
+    const headers = { 'Authorization': 'Bearer <token>','Content - type': 'application / json'}; // auth header with bearer token
+    post('https://reqres.in/api/login' + '/' + emailId + '/' + passwordID, { headers })
+        .then(response => response.json())
+        .then(data => tokenID = data.id);
+
+
+    alert("Successfully logged in")
+
+
+    get('https://reqres.in/api' + '/' + tokenID , { headers })
+        .then(response => response.json())
+        .then(data => {
+
+
+            console.log(data.data)
+
+            data.data.forEach(d => {
+
+                users.push(d.name);
+
+            });
+
+            console.log(users);
+        });
+
+
+}
+
